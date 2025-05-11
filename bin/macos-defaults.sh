81c49bbfbd6e544e9c119d1afac8b21bfd388278
Xavier Decuyper <hi@savjee.be>
2025-05-11 20:18:36 +0200

    Restructure defaults

--- bin/macos-defaults.sh
+++ bin/macos-defaults.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env bash
 
-# Disable automatically rearrange Spaces based on recent use
-defaults write com.apple.dock mru-spaces -bool false
+# ------------------------------------------------------------------------
+#                               DOCK
+# ------------------------------------------------------------------------
 
 # Remove the hiding Dock Delay in OS X
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
-killall Dock
 
-# Disable dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
+# Dock: don't show recent apps (only running apps)
+defaults write com.apple.dock show-recents -bool false
+
+# ------------------------------------------------------------------------
+#                               MISSION CONTROL
+# ------------------------------------------------------------------------
+
+# Disable automatically rearrange Spaces based on recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom right screen corner -> Screensaver
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Don't show recent apps in dock
+defaults write com.apple.dock show-recents -bool false
+
+# ------------------------------------------------------------------------
+#                               OTHERS
+# ------------------------------------------------------------------------
 
 # Disable automatic capitalization as it’s annoying when typing code
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
@@ -30,26 +56,20 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
+# ------------------------------------------------------------------------
+#                               FINDER
+# ------------------------------------------------------------------------
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
 # When performing a search, search the current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Top left screen corner → Mission Control
-defaults write com.apple.dock wvous-tl-corner -int 2
-defaults write com.apple.dock wvous-tl-modifier -int 0
+# Prefill NAS connection when pressing Cmd+K
+defaults write com.apple.finder FXConnectToLastURL -string "smb://xavier@192.168.2.216/"
 
-# Top right screen corner → Desktop
-defaults write com.apple.dock wvous-tr-corner -int 4
-defaults write com.apple.dock wvous-tr-modifier -int 0
-
-# Bottom right screen corner -> Screensaver
-defaults write com.apple.dock wvous-br-corner -int 5
-defaults write com.apple.dock wvous-br-modifier -int 0
-
-# Dock: don't show recent apps (only running apps)
-defaults write com.apple.dock show-recents -bool false
+# Automatically resize columns based on filenames
+defaults write com.apple.finder _FXEnableColumnAutoSizing -bool true
 
 # ------------------------------------------------------------------------
 #                               SAFARI
@@ -74,3 +94,9 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 # Sort Activity Monitor results by CPU usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+# ------------------------------------------------------------------------
+#                            RESTART APPS
+# ------------------------------------------------------------------------
+killall Dock
+killall Finder
